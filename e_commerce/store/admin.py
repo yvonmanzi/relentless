da9b453c572e2331cf7783ff923c32d0f32d986d
yvon <yvonmanzi@gmail.com>
2024-07-13 12:26:19 +0200

    ran git rm --cached . to ensure every ignored thing is not tracked.

--- e_commerce/store/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from store.models import Cart, Order, Product
-
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Cart)
-admin.site.register(Order)
