a112a3fc3e7c42095b255faebc252ecc32640f4d
yvon <yvonmanzi@gmail.com>
2024-07-24 10:01:03 +0200

    serializers for login and registering users

--- e_commerce/authentication/serializers.py
+++ e_commerce/authentication/serializers.py
@@ -1,7 +1,35 @@
 from rest_framework import serializers
+from django.contrib.auth import authenticate
 
-from authentication.models import Customer
-class CustomerSerializer(serializers.ModelSerializer):
+
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = '__all__'
-        model = Customer 
+        model = User 
+        # Does it make sense to do all here?
+        fields = ('id', 'username', 'email')
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = User 
+        fields = ('username', 'password', 'email')
+        extra_kwargs = {'password': {'write_only': True}}
+    def create(self, validated_data):
+        if User.objects.filter(username=validated_data['username']).exists():
+            return serializers.ValidationError({'username': 'Username already exists'})
+        user = User.object.create_user(
+            validated_data['username'],
+            validated_data['email'],
+            validated_data['password']
+        )
+        return user 
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+    def validate(self, attrs):
+        user = authenticate(**attrs)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid credentials")
+
