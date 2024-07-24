8857795a7d09bae9a747f31e02713af4bd01e714
yvon <yvonmanzi@gmail.com>
2024-07-24 11:00:17 +0200

    configure urls for login and register user

--- e_commerce/e_commerce/urls.py
+++ e_commerce/e_commerce/urls.py
@@ -6,14 +6,16 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from store.views import CartViewSet, OrderViewSet, ProductViewSet
-from authentication.views import CustomerViewSet
+# from authentication.views import CustomerViewSet
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
 router.register(r'carts', CartViewSet)
 router.register(r'orders', OrderViewSet)
-router.register(r'customers', CustomerViewSet)
+# router.register(r'customers', CustomerViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('api/', include("authentication.urls"))
 ]
+
