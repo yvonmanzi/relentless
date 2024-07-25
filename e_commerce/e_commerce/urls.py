9eb4bc0a9ffb692c1dd8d9d4d51195e6dd20f697
yvon <yvonmanzi@gmail.com>
2024-07-25 13:47:37 +0200

    added urls for product list view and detail view.

--- e_commerce/e_commerce/urls.py
+++ e_commerce/e_commerce/urls.py
@@ -2,20 +2,30 @@
 URL configuration for e_commerce project.
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 
 from store.views import CartViewSet, OrderViewSet, ProductViewSet
-# from authentication.views import CustomerViewSet
+
+class CustomRouter(DefaultRouter):
+    def get_lookup_regex(self, viewset, lookup_prefix=''):
+        base_regex = super().get_lookup_regex(viewset, lookup_prefix)
+        # Extend the base regex to include the slug
+        return f'(?P<{lookup_prefix}pk>[^/.]+)/(?P<slug>[^/.]+)'
+
+router = CustomRouter()
+router.register(r'products', ProductViewSet, basename='product')
+
 
 router = DefaultRouter()
-router.register(r'products', ProductViewSet)
-router.register(r'carts', CartViewSet)
-router.register(r'orders', OrderViewSet)
+router.register(r'products', ProductViewSet, basename='product')
+router.register(r'carts', CartViewSet, basename='cart')
+router.register(r'orders', OrderViewSet, basename='order')
 # router.register(r'customers', CustomerViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api/', include("authentication.urls"))
+    path('api/', include("authentication.urls", namespace='authentication')),
+    path('api/', include('store.urls', namespace='store'))
 ]
 
