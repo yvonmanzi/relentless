0d098e7a1271d961e3bcdcf8ff0779837db508d6
yvon <yvonmanzi@gmail.com>
2024-08-07 12:37:59 +0200

    factories for category and product

---
+++ e_commerce/store/factories.py
@@ -0,0 +1,28 @@
+import factory  # type: ignore
+from store.models import Category, Product
+from factory.django import DjangoModelFactory # type: ignore
+from django.utils.text import slugify
+
+class CategoryFactory(DjangoModelFactory):
+    class Meta:
+        model = Category
+    
+    name = factory.Faker('word') 
+    slug = factory.LazyAttribute(lambda o: slugify(o.name)) 
+
+class ProductFactory(DjangoModelFactory):
+    class Meta:
+        model = Product
+
+    category = factory.SubFactory(CategoryFactory)
+    name = factory.Faker('word')  # Generates a random word for the product name
+    description = factory.Faker('text', max_nb_chars=200)  # Generates a random text for the description
+    slug = factory.LazyAttribute(lambda o: slugify(o.name))  # Generates a slug from the product name
+    image = factory.django.ImageField(filename='product_image.jpg')  # Generates a sample image file
+    price = factory.Faker('decimal', max_digits=6, decimal_places=2)  # Generates a random price
+    available = factory.Faker('boolean')  # Random boolean for availability
+    created = factory.Faker('date_time_this_year', before_now=True, after_now=False)  # Random creation date
+    updated = factory.Faker('date_time_this_year', before_now=True, after_now=False)  # Random update date
+    
+
+
