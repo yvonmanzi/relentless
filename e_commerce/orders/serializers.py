919c2fc87885d0adf0c3ead4a1f408f46024abc2
yvon <yvonmanzi@gmail.com>
2024-08-07 10:16:30 +0200

    order serializer

---
+++ e_commerce/orders/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from orders.models import Order
+
+class OrderSerializer(serializers.Serializer):
+    class Meta:
+        fields = '__all__'
+        model = Order
+
+    
